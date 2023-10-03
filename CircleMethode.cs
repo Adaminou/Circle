75222a3cf6671c5600fd8dd5b573b8ce662176b9
Essalhi Adam <200537@CCM-Asty-Moulin.local>
2023-10-03 14:41:48 +0200

    oui

---
+++ CircleMethode.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adam_Circle
+{
+    class CircleMethode
+    {
+        // attributs privés par convention, leur nom commence par '_'
+        private double _rayon;
+        public double rayon
+        {
+            get
+            {
+                return _rayon;
+            }
+            set
+            {
+                _rayon = value;
+            }
+        }
+
+        public double CalculAire()
+        {
+            double surface = Math.PI * _rayon * _rayon;
+            return surface;
+        }
+        public double CalculPerimetre()
+        {
+            double perimetre = 2 * Math.PI * _rayon;
+            return perimetre;
+        }
+        public string AfficheInfo()
+        {
+           string phrase = "Le cercle de Rayon" + _rayon + "a un perimetre de" + CalculPerimetre() + "et une aire de" + CalculAire();
+            return phrase;
+        }
+
+    }
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using Adam_Circle;
+
+do
+{
+    Console.WriteLine("Bienvenue dans ce programme sur le cercle");
+    Console.WriteLine("Tapez un rayon pour votre cercle");
+   
+
+    Console.WriteLine("Avec un cercle diminué de moitié :");
+}
+while ();
